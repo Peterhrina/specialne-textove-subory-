8cae3312324bd86638745a4eb258283312c6baa6
jakubkendera <jakubkendera2006@gmail.com>
2025-09-05 09:12:19 +0200

    hodina

--- skola.py
+++ skola.py
@@ -1,1 +1,15 @@
-#
+#odrbavacia hra s benfordovym zakonom-vygeneruje 2 nahodne cisla od 100 do 1000. jedno cislo umocnim na druhe
+#zistit prvu cifru tohto cisla a nasledne pocitat jej vyskyt a vypisat absolutnu aj relativnu
+import random
+pocetnost = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+pocet_opakovani = 10000
+for i in range(pocet_opakovani):
+    x = random.randrange(100, 1001)
+    y = random.randrange(100, 1001)
+    vysledok = x**y
+    cifra = int(str(vysledok)[0])
+    pocetnost[cifra] += 1
+for i in pocetnost:
+    print(round((pocetnost[i]/pocet_opakovani)*100, 2), end = ',')
+
+print(pocetnost)
